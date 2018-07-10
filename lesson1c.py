f49c0b6df659a91df194ce5b503e39888e38f304
heynoparking <booboo0590@gmail.com>
2018-07-10 15:23:33 +0800

    change lesoon1c

---
+++ lesson1c.py
@@ -0,0 +1,10 @@
+grade = {
+    'Chinese' : 98,
+    'English' : 65,
+    'Math' : 87,
+    'History' : 77,
+    'Physics' : 50,
+    'Chemistry' : 88
+}
+
+print("Physics score = ", grade['Physics'])
