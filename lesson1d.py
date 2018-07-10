9013d5a5ec500060409cbdfd66b954641802d0d0
heynoparking <booboo0590@gmail.com>
2018-07-10 16:29:01 +0800

    change lesson1d

---
+++ lesson1d.py
@@ -0,0 +1,13 @@
+x=input("input")
+y=input("input")
+
+if x>0:
+    if y>0 :
+        print('1')
+    else:
+        print('4')
+else:
+    if y>0:
+        print('2')
+    else:
+        print('3')
