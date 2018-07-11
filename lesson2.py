d9aaf30b572585252f8de153da0212d36755cfa5
heynoparking <booboo0590@gmail.com>
2018-07-11 09:47:04 +0800

    change lesson2

---
+++ lesson2.py
@@ -0,0 +1,21 @@
+a = 1
+while a < 20:
+    print a,
+    a += 2
+print("end")
+
+n = 100
+iterate = 0
+total = 0
+while iterate != n:
+    iterate += 1
+    total += iterate
+print(total)
+
+a=1
+while a<7:
+    if(a%2==0):
+        print(a,"is even")
+    else:
+        print(a,"is odd")
+    a+=1
